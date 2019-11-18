19ef5f119949ac7669a360be5f7c61cb7f71699b
bhasvara <bhasvara@cisco.com>
2019-11-18 13:08:52 +0530

    Renamed the long filename into short names

---
+++ test123.py
@@ -0,0 +1,15 @@
+for count in range(1, 10):
+    print("Welcome to Python {}", count)
+print()
+
+for count in range(0, 10):
+    print("Welcome to Python {}", count)
+print()
+
+for count in range(1, 11):
+    print("Welcome to Python {}", count)
+print()
+
+for count in range(1, 12):
+    print("Welcome to Python {}", count)
+print()
